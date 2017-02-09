e08c51fdc412eb1f44ba3bce5fdac3fb1f303b54
Mason <masonlgoetz@gmail.com>
2017-02-09 14:50:07 -0700

    pushing for the boys

---
+++ src/actions/movies.js
@@ -0,0 +1,4 @@
+export const userSearch = (term) => ({
+  type: 'USER_SEARCH',
+  term
+})
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,7 +3,6 @@ import { GridList } from 'material-ui/GridList'
 import { MovieCard } from './MovieCard'
 
 const renderMovies = (movies) => {
-  console.log('movies', movies);
   return movies.map((movie, i) => {
     return <MovieCard movie={ movie } key={ `${movie.Title}${i}` } />
   })
